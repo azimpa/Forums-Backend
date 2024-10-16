8a2b9ee61bdef80716a7dc7c0fd1067909c7e846
azimpa <muhammedasim.pa@gmail.com>
2024-10-16 12:21:32 +0530

    new

--- forums/views.py
+++ forums/views.py
@@ -3,6 +3,8 @@ from .models import Category, Thread, Responses, Tag
 from rest_framework import status
 from rest_framework.response import Response
 from .serializers import CategorySerializer, ThreadSerializer, ResponseSerializer, TagSerializer
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -13,13 +15,9 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ThreadViewSet(viewsets.ModelViewSet):
     queryset = Thread.objects.all()
     serializer_class = ThreadSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category']
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        category_id = self.request.query_params.get('category', None)
-        if category_id:
-            queryset = queryset.filter(category_id=category_id)
-        return queryset
 
     def create(self, request, *args, **kwargs):
         user = request.user
--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
+from forums.serializers import UserSerializer
 
 
 @api_view(['POST'])
@@ -15,16 +16,14 @@ def register_user(request):
         username = request.data.get('username')
         email = request.data.get('email')
         password = request.data.get('password')
+
         if User.objects.filter(username=username).exists():
             return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
-        user = User.objects.create_user(
-            username=username, email=email, password=password)
+
+        user = User.objects.create_user(username=username, email=email, password=password)
         token, _ = Token.objects.get_or_create(user=user)
-        user_data = {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email,
-        }
+
+        user_data = UserSerializer(user).data
         return Response({'token': token.key, 'user': user_data}, status=status.HTTP_201_CREATED)
 
 
@@ -35,15 +34,12 @@ def user_login(request):
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username, password=password)
+
         if user:
             login(request, user)
             token, _ = Token.objects.get_or_create(user=user)
 
-            user_data = {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email,
-            }
+            user_data = UserSerializer(user).data
             return Response({'token': token.key, 'user': user_data}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
